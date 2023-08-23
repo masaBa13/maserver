aa09b58e6b3b6873e748a93a4cefb75a17cdf21d
Masa <masabasco13@outlook.jp>
2023-08-23 14:20:19 +0900

    first commit

---
+++ portfolio/src/views/HomeView.vue
@@ -0,0 +1,319 @@
+<template>
+  <v-app id="home">
+    <NavBar />
+    <v-container fluid>
+        <div class="head">
+          <v-row>
+            <v-col cols="5">
+              <div style="position: relative" class="mt-16">
+                <h1 class="text-grey">Hello,</h1>
+                <h1 class="text-white">I'm Lara Elizabeth</h1>
+                <span class="text-grey">Web Designer & Developer</span><br />
+                <v-btn tile dark class="text-yellow mt-8" variant="outlined">Contact me</v-btn>
+              </div>
+            </v-col>
+            <v-col cols="2">
+              <div style="position: absolute;z-index: 9999;bottom: 0;margin-right: auto;left: 0;right: 0;text-align: center;">
+                <v-icon>fas fa-angle-double-down</v-icon>
+              </div>
+            </v-col>
+            <v-col cols="5">
+              <div style="position: relative;z-index: 9999;" class="mt-16">
+                  <v-img src="i1.png" contain max-height="300"></v-img>
+              </div>
+            </v-col>
+          </v-row>
+        </div>
+        <v-col clols="12" class="mt-16" id="about">
+          <div>
+            <r-row>
+              <v-col cols="12" sm="6">
+                <div class="egg">
+                  <v-img src="i2.png" max-height="300"></v-img>
+                </div>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <h5 class="mt-16">About Me</h5>
+                <div style="width: 120px;">
+                  <v-slider v-model="slider2" color="yellow" label="track-color">
+                  
+                  </v-slider>
+                </div>
+                <h4 class="mt-n4">I Am a Creative Web Design</h4>
+                <h4>And Deveroper</h4>
+                <p class="text-grey">Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                  Rerum neque quae vero tenetur non voluptate incidunt officia veritatis 
+                  reiciendis beatae doloribus nemo, dolorum voluptas esse nam consequatur 
+                  suscipit praesentium aut.
+
+                </p>
+              <br />
+              <p class="text-grey">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Fugit, inventore! 
+                Nobis sit accusantium dicta architecto itaque quisquam dolor illo quos, 
+                dolorum eum illum deserunt soluta? Sed nihil sapiente atque nobis!
+              </p>
+              <v-btn tile dark color="yellow" class="mt-4">
+                Downlord Resume
+              </v-btn>
+              </v-col>
+            </r-row>
+          </div>
+        </v-col>
+        <div class="text-center mt-4">
+          <h2>What We Do</h2>
+          <div style="width: 120px; margin: 0 auto">
+            <v-slider v-model="slider2" color="yellow" label="track-color"></v-slider>
+          </div>
+        </div>
+        <v-col cols="12" class="padd" id="portfolio">
+          <div class="first" id="project">
+            <v-row>
+              <v-col cols="12">
+                <div class="child">
+                  <v-btn icon="fas fa-laptop" color="#F8DF7E" class="text-white"></v-btn>
+                  <h3 class="ml-3 mt-4">Web Design</h3>
+                  <p class="text-grey ml-3 mt-4 text-caption">
+                    Lorem, ipsum dolor sit amet <br/> consetvu adipisiving
+                    <br/>consumer adding
+                  </p>
+                </div>
+                <div class="child">
+                  <v-btn icon="fas fa-mobile-alt" color="#F8DF7E" class="text-white"></v-btn>
+                  <h3 class="ml-3 mt-4">App Design</h3>
+                  <p class="text-grey ml-3 mt-4 text-caption">
+                    Lorem, ipsum dolor sit amet <br/> consetvu adipisiving
+                    <br/>consumer adding
+                  </p>
+                </div>
+                <div class="child">
+                  <v-btn icon="fas fa-camera" color="#F8DF7E" class="text-white"></v-btn>
+                  <h3 class="ml-3 mt-4">Photography</h3>
+                  <p class="text-grey ml-3 mt-4 text-caption">
+                    Lorem, ipsum dolor sit amet <br/> consetvu adipisiving
+                    <br/>consumer adding
+                  </p>
+                </div>
+              </v-col>
+            </v-row>
+            <v-divider></v-divider>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="12" id="services">
+          <div class="d-flex justify-center mb-6">
+            <v-btn color="FBDF7E" class="mr-2">ALL</v-btn>
+            <v-btn class="mr-2" variant="tonal">Web Design</v-btn>
+            <v-btn class="mr-2" variant="tonal">Fron Design</v-btn>
+            <v-btn class="mr-2" variant="tonal">Photography</v-btn>
+            <v-btn variant="tonal">Illustlation</v-btn>
+          </div>
+        </v-col>
+        <v-col cols="12" class="imgHober">
+          <v-row class="fill-height" align="center" justify="center">
+            <template v-for="(item, i) in items" :key="i">
+              <v-col cols="12" md="4">
+                <v-hover v-slots="{isHoving, progs}">
+                  <v-card :elevation="isHoving ? 12 : 2" :class="{'on-hover' : isHovering}" v-bind="props">
+                    <v-img :src="item.img" height="225px" cover></v-img>
+                  </v-card>
+                </v-hover>
+              </v-col>
+            </template>
+          </v-row>
+        </v-col>
+        <v-col cols="12" sm="12">
+          <div class="d-flex justify-center mb-6">
+            <v-btn colo="white" class="mt-4">Load more</v-btn>
+          </div>
+        </v-col>
+        <v-col cols="12" id="page">
+          <div class="pre">
+            <v-row>
+              <v-col cols="12" sm="4">
+                <v-card class="mw-auto" max-width="344" height="">
+                  <v-img src="i10.png" haight="200px" cover></v-img>
+                  <v-card-title>We provide you the best</v-card-title>\
+                  <v-card-subtitle>
+                    by AAE IdeaPro                   
+                  </v-card-subtitle>
+                  <v-card-text>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ut voluptatibus magnam commodi possimus animi, deleniti labore blanditiis, incidunt assumenda sequi laborum amet, eveniet inventore aspernatur excepturi minus ipsum officia?
+                  </v-card-text> 
+                </v-card>
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+        <v-col cols="12" id="biog">
+          <div class="hire">
+            <v-row>
+              <v-col cols="12" sm="8">
+                <h1 class="mt-9">Hire me for your awesome project</h1>
+                <p class="text-grey">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. A eaque minus autem ab, voluptatem sint sunt vitae sapiente velit placeat. Officiis quod fuga ratione? Incidunt rerum dolores eos totam harum!
+                </p>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-btn color="#FBDF7E" class="mt-15">Hire me</v-btn>
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="12" class="px-16" id="contact">
+          <v-row>
+            <v-col cols="12" sm="4">
+              <div class="child">
+                <h1>Contact info.</h1>
+                <v-btn icon="fas fa-map-marker-alt" class="mt-10" variant="outlined"></v-btn><br/>
+                <span class="text-caption">Your Street SYO, Aera Dream</span> <br/>
+                <v-btn icon="fas fa-phone-alt" class="mt-10" variant="outlined"></v-btn><br/>
+                <span class="text-caption">080-3327-2551</span><br/>
+                <span class="text-caption">080-3327-2551</span><br/>
+                <v-btn icon="fas fa-phone-alt" class="mt-10" variant="outlined"></v-btn><br/>
+                <span class="text-caption">skrideapro@fun.ac.jp</span><br/>
+                <span class="text-caption">skrideapro@fun.ac.jp</span><br/>
+              </div>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" sm="8">
+          <h1 class="mt-8">Send your message</h1>
+          <v-divider></v-divider>
+            <span class="text-caption">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Beatae doloremque dolores vel deleniti quaerat rem 
+              odit culpa numquam distinctio non vitae, maxime quidem expedita? 
+              Animi vero quam incidunt at consequuntur!
+            </span>
+            <v-row class="mt-10">
+              <v-col cols="12" sm="6">
+                <v-text-field label="Name" persistent-hint variant="outlined"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field label="Phone No" persistent-hint variant="outlined"></v-text-field>
+              </v-col>
+
+            </v-row>
+            <v-textarea label="Message" persistent-hint variant="outlined"></v-textarea>
+            <v-btn color="#FBDF7E" class="mt-2">Submit Now</v-btn>
+        </v-col>
+    </v-container>
+    <Footerview/>
+  </v-app>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import NavBar from '@/components/NavBar.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  setup(){
+    return{
+     slider2: 50,
+
+     items: [
+      {
+        img: "i3.jpg",
+      },
+      {
+        img: "i4.jpg",
+      },
+      {
+        img: "i5.jpg",
+      },
+      {
+        img: "i6.jpeg",
+      },
+      {
+        img: "i9.jpg",
+      },
+      {
+        img: "i8.jpg",
+      },
+     ]
+    }
+  },
+
+  components: {
+    NavBar
+},
+});
+</script>
+<style scoped>
+.v-container{
+  padding: 16px 0;
+}
+.head {
+  position: relative; 
+  text-align: center;
+  padding: 12px;
+  margin-bottom: 6px;
+  height: 400px;
+  width: 100%;
+  color: white;
+}
+.head:before{
+  content: " ";
+  position: absolute;
+  top: 0;
+  left:  0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, 6deg);
+}
+
+.head::after{
+  content: " ";
+  position: absolute;
+  top: 0;
+  right:  0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, -6deg);
+}
+
+.egg{
+  display: block;
+  margin-left: 100px;
+  margin-top: 50px;
+  width: 356px;
+  height: 300px;
+  background-color: #FBDF7E;
+  border-radius: 50% 50% 50% 50% / 60% 60% 40% 0%;
+}
+.first{
+  width: 100%;
+  height: 280px;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.child{
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  margin-right: 8px;
+}
+.imgHover{
+  padding:  0 200px;
+}
+.pre{
+  width: 100%;
+  height: 380px;
+  text-align: center;
+  padding: 0 200px;
+  background-color: #f5f5f5;
+}
+.hire{
+  width: 100%;
+  height: 200px;
+  padding: 0 200px;
+  background-color: #e9e9e9;
+  margin-top: -24px;
+}
+</style>
